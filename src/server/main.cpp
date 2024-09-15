732e6acc68dab94fa45ee0dec3ca57f58af0d6fd
Redchin Daniil <redchindaniil@gmail.com>
2024-09-16 00:09:39 +0500

    Added basic socket code

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+#include <sys/socket.h>
+#include <unistd.h>
+
 int main() {
     std::cout << "Hello, world!" << std::endl;
+
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0); // IPv4, TCP
+
+    std::cout << "Socket created." << std::endl;
+
+    close(socket_fd);
+    std::cout << "Socket freed." << std::endl;
+
+    return 0;
 }
